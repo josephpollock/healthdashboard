31732dc388e16c9bd15a996d40171c12d7c8df64
Pollock <joseph.pollock@accenture.com>
2017-09-11 15:39:08 -0700

    added sidebar and other components

--- healthdashboard/src/app/app.module.ts
+++ healthdashboard/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { CasesComponent } from './cases/cases.component';
 import { CaseblockComponent } from './caseblock/caseblock.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CaseListComponent } from './case-list/case-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     CasesComponent,
-    CaseblockComponent
+    CaseblockComponent,
+    SidebarComponent,
+    CaseListComponent
   ],
   imports: [
     BrowserModule
