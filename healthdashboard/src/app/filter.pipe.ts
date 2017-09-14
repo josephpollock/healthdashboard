8b697863ef567b483d5250f64e66630384cb4f1d
Pollock <joseph.pollock@accenture.com>
2017-09-14 08:50:43 -0700

    fixed filter matching

--- healthdashboard/src/app/filter.pipe.ts
+++ healthdashboard/src/app/filter.pipe.ts
@@ -11,6 +11,6 @@ export class FilterPipe implements PipeTransform {
       if (!items) return [];
       if (!value || value.length == 0) return items;
       return items.filter(it =>
-      it[field].toLowerCase().indexOf(value.toLowerCase()) !=-1);
+      it[field].toLowerCase() == (value.toLowerCase()) );
     }
 }
